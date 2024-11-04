224649e89bea4942192b2f77cbf497d6c619f3d6
Keith <keith.githinji@student.moringaschool.com>
2024-11-04 18:42:49 +0300

    Completed the lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
 
-  function handleAnswer(isCorrect) {
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      const timer = setTimeout(() => {
+        setTimeRemaining(prevTime => prevTime - 1);
+      }, 1000);
+      
+       return () => clearTimeout(timer);
+    } else {
+       setTimeRemaining(10);
+      onAnswered(false);  
+    }
+  }, [timeRemaining, onAnswered]);
+   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
 
+
   const { id, prompt, answers, correctIndex } = question;
 
   return (
-    <>
+    <div>
       <h1>Question {id}</h1>
       <h3>{prompt}</h3>
       {answers.map((answer, index) => {
@@ -25,7 +38,7 @@ function Question({ question, onAnswered }) {
         );
       })}
       <h5>{timeRemaining} seconds remaining</h5>
-    </>
+    </div>
   );
 }
 
